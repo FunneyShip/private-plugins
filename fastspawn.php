cfd625beaa3ae5a2801afce992dfd529f9e15c05
FunneyShip <funneyship@outlook.com>
2014-06-08 22:07:05 -0400

    Create fastspawn.php

---
+++ fastspawn.php
@@ -0,0 +1,48 @@
+<?php
+
+/*
+__PocketMine Plugin__
+name=fastspawn
+description=that was FAST!!
+version=0.0.1
+author=FunneyShip
+class=Spawn
+apiversion=11,12,13
+*/
+
+class Spawn implements Plugin
+{
+    private $api;
+
+    public function __construct(ServerAPI $api, $server = false)
+    {
+        $this->api = $api;
+    }
+
+    public function init()
+    {
+    $this->api->addHandler("player.respawn", array($this, "handle"));
+    $this->api->addHnadler("player.equipment.change", array($this, "handle"));
+    }
+    
+     public function EventHandler($data, $event){
+     switch($event)
+     {
+     case "player.equipment.change":
+     if($data["item"]->getID() == $this->CONFIG["345"])
+     {
+      $this->api->console->run("spawn");
+      }
+      
+      break;
+      
+      case "player.respawn":
+      
+       $this->api->console->run("give " . $issuer->username . "345" . "1 ");
+      
+      break;
+      }
+      }
+     public function __destruct(){
+     }
+     }
